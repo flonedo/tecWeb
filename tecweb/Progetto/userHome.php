aabc143cc2f5f9b43acc118f396fd94314c8f571
magiollo <magiollo@t43.torre.math.unipd.it>
2018-01-08 14:32:08 +0100

    Avanzamentosuddivisione header contenuto e footer

--- tecweb/Progetto/userHome.php
+++ tecweb/Progetto/userHome.php
@@ -4,44 +4,17 @@
     if(!isset($_SESSION["user"])){
     	header("Location:index.php");
     }
-?> 
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it" >
-    <head>   
-	<title> Area Riservata - ScambioLibri </title>
-    	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <meta name="title" content="Scambio Libri Vi" />
-        <meta name="description" content="Sito dedicato allo scambio di libri usati, pensato per la provincia di Vicenza e dintorni" />
-		<link rel="stylesheet" type="text/css" href="style/style.css" media="handheld, screen"/>
-		<link  rel = "stylesheet" type="text/css" href="style/small.css" media= "handheld, screen and (max-width:480px), only screen and (max-device-width:480px)" />
-		<link rel = "stylesheet" type="text/css"  href="style/print.css" media="print"/> 
-     </head>
-     <body>
-     <div id="header">
-     <div id="titolo">
-		 <h1> Scambio Libri VI </h1>
-		 <?php
-         	echo '<h2> La pagina personale di <span id="nome" >'. $_SESSION["user"].'</span> </h2>';
-         ?>
-     </div>
-        <a class="abutt" id="logout" href="logout.php" title="Esci"> Logout </a>
-     </div>
-     <div id="corpo">
-    <div id="where">
-        <p> Ti trovi in: <a href="index.php"> Home </a> - Area Riservata </p>
-     </div>
-     <!--Corpo della pagina -->
-     <h1> Il tuo profilo </h1>
-     	<a href="modProf.php" class= "abutt"> Modifica il tuo profilo </a> <br/>
-        <a href="aggiungiLibro.php" class= "abutt"> Aggiungi un libro da scambiare </a> <br/>
-        <a href="eliminaLibro.php"class= "abutt"> Elimina uno dei libri da te inseriti </a> <br/>
-        <a href="eliminaProfilo.php" class= "abutt"> Elimina il tuo profilo </a> <br/>
-     </div>
-    <div id="footer">
-    	 <img id="imgValidCode" src="images/valid-xhtml10.png" alt="Html validation"/>
-        <img id="imgValidCSS" src="images/vcss-blue.gif" alt ="cssValidation"/>
-        <p> Il sito &egrave; creato come progetto nell'ambito del corso di <a href="http://informatica.math.unipd.it/laurea/tecnologieweb.html" title="Pagina web del corso di Tecnlogie web" >Tecnologie web </a> e non rappresenta quindi alcuna associazione esistente. Francesca Lonedo, Marco Giollo, Luca Allegro - <span xml:lang="en">All right reserved </span></p>
-    </div>
-</body>
-</html>
+	$header = file_get_contents("headerPaginapersonale.html");
+	$footer = file_get_contents("footer.html");
+	$header = str_replace("<!--posizione -->", "Area riservata - Scambio Libri Vi", $header);
+	$con = file_get_contents("contenutoPaginaPersonale.html");
+	$header = str_replace("<!--titolo-->", "Pagina personale - Scambio Libri Vi", $header);
+	$out = $_SESSION["user"];
+	$header = str_replace("<!--sottotitolo-->",$out, $header);
+	echo $header;
+	echo $con;
+	echo $footer;
+ ?>
+ 
 
      
