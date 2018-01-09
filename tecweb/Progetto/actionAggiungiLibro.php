de9fe015f12882438e4ee86921d4bea0c8f47820
gmarco5 <g.marco5896@gmail.com>
2018-01-09 13:32:42 +0100

    divisione comportamento etc

--- tecweb/Progetto/actionAggiungiLibro.php
+++ tecweb/Progetto/actionAggiungiLibro.php
@@ -1,39 +1,11 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
 <?php
     session_start();
-      if(!isset($_SESSION["user"])){
+    //Controllo che sia stato fatto il login in precedenza
+     if(!isset($_SESSION["user"])){
     	header("Location:index.php");
     }
-?>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it" >
-    <head>   
-	<title> Inserimento Libri - ScambioLibriVi </title>
-    	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <meta name="title" content="Scambio Libri Vi" />
-        <meta name="description" content="Sito dedicato allo scambio di libri usati, pensato per la provincia di Vicenza e dintorni" />
-		<link rel="stylesheet" type="text/css" href="style/style.css" media="handheld, screen"/>
-		<link  rel = "stylesheet" type="text/css" href="style/small.css" media= "handheld, screen and (max-width:480px), only screen and (max-device-width:480px)" />
-		<link rel = "stylesheet" type="text/css"  href="style/print.css" media="print"/> 
-     </head>
-     <body>
-     
-	<div id="header">
-     <div id="titolo">
-		 <h1> Scambio Libri VI </h1>
-		 <h2> La più semplice piattaforma per lo scambio dei libri, nella provincia di Vicenza  </h2>
-     </div>
-     
-      <a class="abutt" id="logout" href="logout.php" title="Esci"> Logout </a>
-        
-  </div>
-  	 <div id="where">
-        <p> Ti trovi in: <a href="index.php"> Home </a> - <a href="userHome.php"> Area Riservata </a> - Aggiungi Libro </p>
-     </div>
-    <div id="corpo">
-        <?php
-        session_start();
+
+        $output = "";
         require_once 'connectDB.php';
         $reg = new connectDB();
         $openConnection = $reg -> accessDB();
@@ -61,13 +33,13 @@
                 if($check !== false) {
                     $uploadOk = 1;
                 } else {
-                    echo "File is not an image.";
+                    $output=$output."File is not an image.";
                     $uploadOk = 0;
                 }
             }
             // Check file size
             if ($_FILES["photo"]["size"] > 1500000) {
-                echo "La dimensione massima &egrave; 2 MB. Prova con un file di dimensione inferiore";
+                $output = $output."La dimensione massima &egrave; 2 MB. Prova con un file di dimensione inferiore";
                 $uploadOk = 0;
             }
             // Allow only jpg
@@ -85,15 +57,19 @@
                 $target_file = $target_dir.$id.".jpg";
                 move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file);
                 $r=$connessione->query("UPDATE `copiaLibro` SET foto= '$id.jpg' WHERE codiceLibro='$id';");
-                echo "<p>Il libro &egrave stato inserito correttamente</p>";
+                $output = $output."<p>Il libro &egrave stato inserito correttamente</p>";
             }
-        }
-        ?>    
-    </div>
-    <div id="footer">
-    	 <img id="imgValidCode" src="images/valid-xhtml10.png" alt="Html validation"/>
-        <img id="imgValidCSS" src="images/vcss-blue.gif" alt ="cssValidation"/>
-        <p> Il sito &egrave; creato come progetto nell'ambito del corso di <a href="http://informatica.math.unipd.it/laurea/tecnologieweb.html" title="Pagina web del corso di Tecnlogie web" >Tecnologie web </a> e non rappresenta quindi alcuna associazione esistente. Francesca Lonedo, Marco Giollo, Luca Allegro - <span xml:lang="en">All right reserved </span></p>
-    </div>
-   </body>
-</html>
+        }  
+
+    $header = file_get_contents("headerPaginapersonale.html");
+    $footer = file_get_contents("footer.html");
+    $header = str_replace("<!--posizione -->", "<a href='index.php'> Home </a> - <a href='userHome.php'>Area personale </a> - Elimina profilo", $header);
+    $con = file_get_contents("corpo.html");
+    $con = str_replace("<!--corpo-->", $output, $con);
+    $header = str_replace("<!--titolo-->", "Aggiungi Libro - Scambio Libri Vi", $header);
+    $out = $_SESSION["user"];
+    $header = str_replace("<!--sottotitolo-->",$out, $header);
+    echo $header;
+    echo $con;
+    echo $footer;
+?>
--- tecweb/Progetto/actionEliminaLibro.php
+++ tecweb/Progetto/actionEliminaLibro.php
@@ -1,38 +1,10 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
 <?php
     session_start();
     if(!isset($_SESSION["user"])){
             header("Location:index.php");
         }
-?>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="it" lang="it" >
-    <head>   
-	<title> Eliminazione Libri - ScambioLibriVi </title>
-    	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <meta name="title" content="Scambio Libri Vi" />
-        <meta name="description" content="Sito dedicato allo scambio di libri usati, pensato per la provincia di Vicenza e dintorni" />
-		<link rel="stylesheet" type="text/css" href="style/style.css" media="handheld, screen"/>
-		<link  rel = "stylesheet" type="text/css" href="style/small.css" media= "handheld, screen and (max-width:480px), only screen and (max-device-width:480px)" />
-		<link rel = "stylesheet" type="text/css"  href="style/print.css" media="print"/> 
-     </head>
-     <body>
-     
-	<div id="header">
-     <div id="titolo">
-		 <h1> Scambio Libri VI </h1>
-		 <h2> La più semplice piattaforma per lo scambio dei libri, nella provincia di Vicenza  </h2>
-     </div>
-     
-      <a class="abutt" id="logout" href="logout.php" title="Esci"> Logout </a>
-        
-  </div>
-  	 <div id="where">
-        <p> Ti trovi in: <a href="index.php"> Home </a> - <a href="userHome.php"> Area Riservata </a> - Libri Eliminati </p>
-     </div>
-    <div id="corpo">
-        <?php
-        session_start();
+        $output = "";
         require_once 'connectDB.php';
         $reg = new connectDB();
         $openConnection = $reg -> accessDB();
@@ -48,14 +20,19 @@
                         unlink($photo);
                     $r=$connection->query("DELETE FROM copiaLibro WHERE codiceLibro=$key;");
                 }
-            echo "I libri sono stati eliminati";
+            $output = $output."I libri sono stati eliminati";
         }
-        ?>    
-    </div>
-    <div id="footer">
-    	 <img id="imgValidCode" src="images/valid-xhtml10.png" alt="Html validation"/>
-        <img id="imgValidCSS" src="images/vcss-blue.gif" alt ="cssValidation"/>
-        <p> Il sito &egrave; creato come progetto nell'ambito del corso di <a href="http://informatica.math.unipd.it/laurea/tecnologieweb.html" title="Pagina web del corso di Tecnlogie web" >Tecnologie web </a> e non rappresenta quindi alcuna associazione esistente. Francesca Lonedo, Marco Giollo, Luca Allegro - <span xml:lang="en">All right reserved </span></p>
-    </div>
-   </body>
-</html>
+        
+    
+    $header = file_get_contents("headerPaginapersonale.html");
+    $footer = file_get_contents("footer.html");
+    $header = str_replace("<!--posizione -->", "<a href='index.php'> Home </a> - <a href='userHome.php'>Area personale </a> - Elimina profilo", $header);
+    $con = file_get_contents("corpo.html");
+    $con = str_replace("<!--corpo-->", $output, $con);
+    $header = str_replace("<!--titolo-->", "Aggiungi Libro - Scambio Libri Vi", $header);
+    $out = $_SESSION["user"];
+    $header = str_replace("<!--sottotitolo-->",$out, $header);
+    echo $header;
+    echo $con;
+    echo $footer;
+?>
