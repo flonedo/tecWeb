8127f685a21010ad122d716b0eee16f3ff835f95
gmarco5 <g.marco5896@gmail.com>
2018-02-09 08:33:13 +0100

    Messo a posto link ballerino

--- tecweb/Progetto/chisiamo.php
+++ tecweb/Progetto/chisiamo.php
@@ -13,7 +13,7 @@
 		$log = '<a class="abutt" id="aPers" href="userHome.php" title="Vai alla tua pagina personale"> Pagina Personale </a> </br>
                   <a class="abutt" id="logout" href="logout.php" title="Esci"> Logout </a>';
         $aiuti = "<a href=\"logout.php\" class=\"aiuti\"> Fai il logout </a>
-                 <a href=\"userHome.php\"> Vai alla tua aera personale </a>";
+                 <a href=\"userHome.php\" class=\"aiuti\"> Vai alla tua aera personale </a>";
 	}
     $header = str_replace("<!--aiuto-->", $aiuti, $header);
 	$header = str_replace("<!-- login -->", $log, $header);
